060d182e90dec1771c3191b860afa320ba2ff1ef
Benjamin Wheeler <benjaminwheeler@ufl.edu>
2019-10-27 16:50:34 -0400

    Fixed circle

--- RTInAWeekend/vec3.h
+++ RTInAWeekend/vec3.h
@@ -37,27 +37,9 @@ struct vec3 {
 	float e[3];
 };
 
-inline istream& operator>>(istream & input, vec3 & t) {
-	input >> t.e[0] >> t.e[1] >> t.e[2];
-	return input;
-}
 
-inline ostream& operator<<(ostream & output, vec3 & t) {
-	output << t.e[0] << " " << t.e[1] << " " << t.e[2];
-	return output;
-}
 
-inline void vec3::make_unit_vector() {
-	float k = 1.f / sqrt(e[0] * e[0] + e[1] * e[1] + e[2] * e[2]);
-	e[0] *= k;
-	e[1] *= k;
-	e[2] *= k;
-}
-
-inline vec3 unit_vector(const vec3& v) {
-	return v / v.length();
-}
-
+//-----Addition operators-----//
 inline vec3 operator+(const vec3 &v1, const vec3 &v2) {
 	return vec3(v1.e[0] + v2.e[0], v1.e[1] + v2.e[1], v1.e[2] + v2.e[2]);
 }
@@ -66,30 +48,25 @@ inline vec3 operator-(const vec3 &v1, const vec3 &v2) {
 	return vec3(v1.e[0] - v2.e[0], v1.e[1] - v2.e[1], v1.e[2] - v2.e[2]);
 }
 
-inline vec3 operator*(float t, const vec3 &v) {
+//-----Multiplication operators-----//
+inline vec3 operator*(const float t, const vec3 &v) {
 	return vec3(t * v.e[0], t * v.e[1], t * v.e[2]);
 }
 
-inline vec3 operator*(const vec3 &v, float t) {
+inline vec3 operator*(const vec3 &v, const float t) {
 	return vec3(t * v.e[0], t * v.e[1], t * v.e[2]);
 }
 
-inline vec3 operator/(const vec3& v, float t) {
+//-----Division operators-----//
+inline vec3 operator/(const vec3& v1, const vec3& v2) {
+	return vec3(v1.e[0] / v2.e[0], v1.e[1] / v2.e[1], v1.e[2] / v2.e[2]);
+}
+
+vec3 operator/(const vec3 &v, const float t) {
 	return vec3(v.e[0] / t, v.e[1] / t, v.e[2] / t);
 }
 
-inline float dot(const vec3& v1, const vec3& v2) {
-	return v1.e[0] * v2.e[0] + v1.e[1] * v2.e[1] + v1.e[2] * v2.e[2];
-}
-
-inline vec3 cross(const vec3& v1, const vec3& v2) {
-	return vec3(
-		v1.e[1] * v2.e[2] - v1.e[2] * v2.e[1],
-		v1.e[2] * v2.e[0] - v1.e[0] * v2.e[2], //Flipped with - sign in tutorial 
-		v1.e[0] * v2.e[1] - v1.e[1] * v2.e[0]
-	);
-}
-
+//-----Assignment operators-----//
 inline vec3& vec3::operator+=(const vec3& rhs) {
 	e[0] += rhs.e[0];
 	e[1] += rhs.e[1];
@@ -118,6 +95,7 @@ inline vec3& vec3::operator/=(const vec3 &rhs) {
 	return *this;
 }
 
+//Scalar assignments
 inline vec3& vec3::operator*=(const float t) {
 	e[0] *= t;
 	e[1] *= t;
@@ -133,3 +111,37 @@ inline vec3& vec3::operator/=(const float t) {
 	return *this;
 }
 
+//Miscellaneous operators
+vec3 unit_vector(vec3 v) {
+	return v / v.length();
+}
+
+inline float dot(const vec3& v1, const vec3& v2) {
+	return v1.e[0] * v2.e[0] + v1.e[1] * v2.e[1] + v1.e[2] * v2.e[2];
+}
+
+inline vec3 cross(const vec3& v1, const vec3& v2) {
+	return vec3(
+		v1.e[1] * v2.e[2] - v1.e[2] * v2.e[1],
+		v1.e[2] * v2.e[0] - v1.e[0] * v2.e[2], //Flipped with - sign in tutorial 
+		v1.e[0] * v2.e[1] - v1.e[1] * v2.e[0]
+	);
+}
+
+inline void vec3::make_unit_vector() {
+	float k = 1.0 / sqrt(e[0] * e[0] + e[1] * e[1] + e[2] * e[2]);
+	e[0] *= k;
+	e[1] *= k;
+	e[2] *= k;
+}
+
+//Input/output
+inline istream& operator>>(istream& input, vec3& t) {
+	input >> t.e[0] >> t.e[1] >> t.e[2];
+	return input;
+}
+
+inline ostream& operator<<(ostream& output, vec3& t) {
+	output << t.e[0] << " " << t.e[1] << " " << t.e[2];
+	return output;
+}
