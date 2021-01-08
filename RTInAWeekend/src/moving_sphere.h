04cfd4699910d5858859d4faa7ae18264bfcdce9
Benjamin Wheeler <benjaminwheeler0510@gmail.com>
2021-01-08 13:42:21 -0500

    refactored hit() to match example code

--- RTInAWeekend/src/moving_sphere.h
+++ RTInAWeekend/src/moving_sphere.h
@@ -7,20 +7,20 @@ public:
 
 	moving_sphere() {}
 	moving_sphere(point3 cen0, point3 cen1, double t0, double t1, float r, shared_ptr<material> m)
-		: center0(cen0), center1(cen1), time0(t0), time1(t1), radius(r), mat_ptr(m) {};
+		: center0(cen0), center1(cen1), time0(t0), time1(t1), radius(r), material(m) {}
 
-	virtual bool hit(const ray& r, float tmin, float tmax, hit_record& rec) const;
+	virtual bool hit(const ray& r, double tmin, double tmax, hit_record& rec) const override;
 
 	point3 center(double time) const;
 
 private:
 	point3 center0, center1;
 	double time0 = 0, time1 = 0;
-	float radius = 0;
-	shared_ptr<material> mat_ptr;
+	double radius = 0;
+	shared_ptr<material> material;
 };
 
-bool moving_sphere::hit(const ray& r, float tmin, float tmax, hit_record& rec) const
+bool moving_sphere::hit(const ray& r, double tmin, double tmax, hit_record& rec) const
 {
 	//Calculate discriminant
 	/*2's and 4's have been commented out, as they cancel out
@@ -28,45 +28,29 @@ bool moving_sphere::hit(const ray& r, float tmin, float tmax, hit_record& rec) c
 	in understanding the math, so I will leave the comments in for now.*/
 	vec3 oc = r.origin() - center(r.time());
 
-	const float a = dot(r.direction(), r.direction());
-	const float half_b = dot(oc, r.direction()); // Represents half of b, since it's normally x2.
-	const float c = oc.length_squared() - radius * radius;
+	const float a = dot(r.direction(), r.direction()),
+		half_b = dot(oc, r.direction()), // Represents half of b, since it's normally x2.
+		c = oc.length_squared() - radius * radius;
+	
 	const float discriminant = half_b * half_b - /* 4 * */ a * c;
 
-	if (discriminant > 0) {
-		// An intersection (or two) between r and the sphere exists.
+	auto sqrtd = sqrt(discriminant);
 
-		float root = sqrt(discriminant);
-
-		// Test closer intersection first
-		float temp = (-half_b - root) / a; // a normally x2
-		// Check if it's valid (within tmin and tmax)
-		if (temp < tmax && temp > tmin) {
-			rec.t = temp;
-			rec.p = r.at(rec.t);
-
-			vec3 outward_normal = (rec.p - center(r.time())) / radius;
-			rec.set_face_normal(r, outward_normal);
-
-			rec.material = mat_ptr;
-			return true;
-		}
-
-		// If the closer intersection is invalid, try the farther one.
-		temp = (-half_b + root) / a;
-		// Check validity
-		if (temp < tmax && temp > tmin) {
-			rec.t = temp;
-			rec.p = r.at(rec.t);
-
-			vec3 outward_normal = (rec.p - center(r.time())) / radius;
-			rec.set_face_normal(r, outward_normal);
-
-			rec.material = mat_ptr;
-			return true;
-		}
+	// Find the nearest root that lies in the acceptable range.
+	auto root = (-half_b - sqrtd) / a;
+	if (root < tmin || tmax < root) {
+		root = (-half_b + sqrtd) / a;
+		if (root < tmin || tmax < root)
+			return false;
 	}
-	return false;
+
+	rec.t = root;
+	rec.p = r.at(rec.t);
+	vec3 outward_normal = (rec.p - center(r.time())) / radius;
+	rec.set_face_normal(r, outward_normal);
+	rec.material = material;
+
+	return true;
 }
 
 point3 moving_sphere::center(double time) const
--- RTInAWeekend/src/sphere.h
+++ RTInAWeekend/src/sphere.h
@@ -5,14 +5,23 @@
 class sphere : public hittable {
 public:
 	sphere() {}
-	sphere(point3 center, float radius, shared_ptr<material> material) : center(center), radius(radius), material(material) {}
+	sphere(point3 center, double radius, shared_ptr<material> material)
+		: center(center), radius(radius), material(material) {}
+
+	virtual bool hit(const ray& r, double tmin, double tmax, hit_record& rec) const override;
 
 private:
 	point3 center;
-	float radius = 0;
+	double radius;
 	shared_ptr<material> material;
+	
+	static void get_sphere_uv(const point3& p, double& u, double& v) {
+		auto theta = acos(-p.y());
+		auto phi = atan2(-p.z(), p.x()) + PI;
 
-	virtual bool hit(const ray& r, float tmin, float tmax, hit_record& rec) const;
+		u = phi / (2 * PI);
+		v = theta / PI;
+	}
 
 	//virtual bool bounding_box(float t0, float t1, aabb& output_box) const;
 };
@@ -24,57 +33,40 @@ private:
 //	return true;
 //}
 
-bool sphere::hit(const ray& r, float tmin, float tmax, hit_record& rec) const {
+bool sphere::hit(const ray& r, double tmin, double tmax, hit_record& rec) const {
 	vec3 oc = r.origin() - center;
 
 	//Calculate discriminant
 	/*2's and 4's have been commented out, as they cancel out
 	in the grand scheme of things. However, they are helpful
 	in understanding the math, so I will leave the comments in for now.*/
-	const float a = dot(r.direction(), r.direction());
-	const float half_b = dot(oc, r.direction()); // Represents half of b, since it's normally x2.
-	const float c = dot(oc, oc) - radius * radius;
+	const float a = r.direction().length_squared(),
+		half_b = dot(oc, r.direction()), // Represents half of b, since it's normally *2.
+		c = oc.length_squared() - radius * radius;
+
 	const float discriminant = half_b * half_b - /* 4 * */ a * c;
+	if (discriminant < 0) return false;
 
-	if (discriminant > 0) {
-		//An intersection (or two) between r and the sphere exists.
 
-		float root = sqrt(discriminant);
+	float sqrtd = sqrt(discriminant);
 
-		//Test closer intersection first
-		float temp = (-half_b - root) / a; // a normally x2
-		//Check if it's valid (within tmin and tmax)
-		if (temp < tmax && temp > tmin) {
-			rec.t = temp;
-			rec.p = r.at(rec.t);
+	// Find the nearest root that lies in the acceptable range.
+	auto root = (-half_b - sqrtd) / a;
 
-			vec3 outward_normal = (rec.p - center) / radius;
-			rec.set_face_normal(r, outward_normal);
-
-			rec.material = material;
-			return true;
-		}
-
-		//If the closer intersection is invalid, try the farther one.
-		temp = (-half_b + root) / a;
-		//Check validity
-		if (temp < tmax && temp > tmin) {
-			rec.t = temp;
-			rec.p = r.at(rec.t);
-
-			vec3 outward_normal = (rec.p - center) / radius;
-			rec.set_face_normal(r, outward_normal);
-
-			rec.material = material;
-			return true;
-		}
+	if (root < tmin || root > tmax) {
+		root = (-half_b + sqrtd) / a;
+		if (root < tmin || root > tmax)
+			return false;
 	}
-	return false;
+
+	rec.t = root;
+	rec.p = r.at(rec.t);
+	vec3 outward_normal = (rec.p - center) / radius;
+	rec.set_face_normal(r, outward_normal);
+	get_sphere_uv(outward_normal, rec.u, rec.v);
+	rec.material = material;
+
+	return true;
 }
 
-void get_sphere_uv(const vec3& p, double& u, double& v) {
-	auto phi = atan2(p.z(), p.x());
-	auto theta = asin(p.y());
-	u = 1 - (phi + PI) / (2 * PI);
-	v = (theta + PI / 2) / PI;
-}
+
