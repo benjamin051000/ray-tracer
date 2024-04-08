8b43df833ab5609400e2728f2ca39757445ba07c
Benjamin Wheeler <benjaminwheeler0510@gmail.com>
2024-04-08 08:54:49 -0400

    added some `constexpr` and lots of `[[nodiscard]]`.

--- inc/vec3.hpp
+++ inc/vec3.hpp
@@ -1,22 +1,29 @@
 #pragma once
 #include <iostream>
+#include <cmath>
 
 
-class vec3 {
-public:
+struct vec3 {
 	//Constructors
 	vec3();
-	vec3(double e0, double e1, double e2);
+	vec3(const double e0, const double e1, const double e2);
 
 	// getters
-	double x() const;
-	double y() const;
-	double z() const;
+	constexpr double x() const { return e[0]; }
+	constexpr double y() const { return e[1]; }
+	constexpr double z() const { return e[2]; }
+
 
 	//General operator overloading
-	vec3 operator-() const;
-	double operator[](int i) const;
-	double& operator[](int i); // Not const because the reference will allow changes
+	[[nodiscard]] vec3 operator-() const; 
+
+	[[nodiscard]] constexpr double operator[](int i) const { 
+		return e[i];
+	}
+
+	[[nodiscard]] constexpr double& operator[](int i) {
+		return e[i];
+	}
 
 	//Assignment operator overloading
 	vec3& operator+=(const vec3& rhs);
@@ -28,15 +35,25 @@ public:
 	vec3& operator*=(const double t);
 	vec3& operator/=(const double t);
 
-	double length() const;
-	double length_squared() const;
+
+	[[nodiscard]] constexpr double length() const {
+		return sqrt(length_squared());
+	}
+
+	[[nodiscard]] constexpr double length_squared() const {
+		return e[0] * e[0] + e[1] * e[1] + e[2] * e[2];
+	}
+
 	//void make_unit_vector();
 
-	bool near_zero() const;
+	[[nodiscard]] constexpr bool near_zero() const {
+		constexpr auto s = 1e-8;
+		return (fabs(e[0]) < s) && (fabs(e[1]) < s) && (fabs(e[2]) < s);
+	}
 
 	static vec3 random();
 
-	static vec3 random(double min, double max);
+	static vec3 random(const double min, const double max);
 
 	//Stores coordinates
 	double e[3];
@@ -47,40 +64,42 @@ using point3 = vec3;
 using color = vec3;
 
 /*-----Addition/Subtraction operators-----*/
-vec3 operator+(const vec3& u, const vec3& v);
+[[nodiscard]] vec3 operator+(const vec3& u, const vec3& v);
 
-vec3 operator-(const vec3& u, const vec3& v);
+[[nodiscard]] vec3 operator-(const vec3& u, const vec3& v);
 
 /*-----Multiplication operators-----*/
-vec3 operator*(double t, const vec3& v);
+[[nodiscard]] vec3 operator*(const double t, const vec3& v);
 
-vec3 operator*(const vec3& v, double t);
+[[nodiscard]] vec3 operator*(const vec3& v, double t);
 
-vec3 operator*(const vec3& u, const vec3& v);
+[[nodiscard]] vec3 operator*(const vec3& u, const vec3& v);
 
 /*-----Division operators-----*/
-vec3 operator/(const vec3& v1, const vec3& v2);
+[[nodiscard]] vec3 operator/(const vec3& v1, const vec3& v2);
 
-vec3 operator/(vec3 v, double t);
+[[nodiscard]] vec3 operator/(vec3 v, double t);
 
 //Miscellaneous operators
-vec3 unit_vector(vec3 v);
+[[nodiscard]] vec3 unit_vector(vec3 v);
 
-double dot(const vec3& u, const vec3& v);
+[[nodiscard]] constexpr double dot(const vec3& u, const vec3& v) {
+	return u[0] * v[0] + u[1] * v[1] + u[2] * v[2];
+}
 
-vec3 cross(const vec3& u, const vec3& v);
+[[nodiscard]] vec3 cross(const vec3& u, const vec3& v);
 
 // cout
 std::ostream& operator<<(std::ostream& output, vec3& t);
 
 /*----------Random Vector Generators----------*/
-vec3 random_in_unit_disk();
+[[nodiscard]] vec3 random_in_unit_disk();
 
-vec3 random_in_unit_sphere();
+[[nodiscard]] vec3 random_in_unit_sphere();
 
-vec3 random_unit_vector();
+[[nodiscard]] vec3 random_unit_vector();
 
-vec3 reflect(const vec3& v, const vec3& n);
+[[nodiscard]] vec3 reflect(const vec3& v, const vec3& n);
 
-vec3 refract(const vec3& uv, const vec3& n, double etai_over_etat);
+[[nodiscard]] vec3 refract(const vec3& uv, const vec3& n, const double etai_over_etat);
 
--- src/vec3.cpp
+++ src/vec3.cpp
@@ -5,30 +5,11 @@
 // Constructors
 vec3::vec3() : e{ 0, 0, 0 } {}
 
-vec3::vec3(double e0, double e1, double e2) : e{ e0, e1, e2 } {}
+vec3::vec3(const double e0, const double e1, const double e2) : e{ e0, e1, e2 } {}
 
-
-double vec3::x() const { return e[0]; }
-double vec3::y() const { return e[1]; }
-double vec3::z() const { return e[2]; }
-
-vec3 vec3::operator-() const { 
+[[nodiscard]] vec3 vec3::operator-() const { 
 	return vec3(-e[0], -e[1], -e[2]); 
 }
-
-double vec3::operator[](int i) const { 
-	return e[i];
-}
-
-double& vec3::operator[](int i) {
-	return e[i];
-}
-
-bool vec3::near_zero() const {
-	const auto s = 1e-8;
-	return (fabs(e[0]) < s) && (fabs(e[1]) < s) && (fabs(e[2]) < s);
-}
-
 
 /*-----Assignment operators-----*/
 vec3& vec3::operator+=(const vec3& rhs) {
@@ -71,20 +52,12 @@ vec3& vec3::operator/=(const double t) {
 	return *this *= 1 / t;
 }
 
-double vec3::length() const {
-	return sqrt(length_squared());
-}
-
-double vec3::length_squared() const {
-	return e[0] * e[0] + e[1] * e[1] + e[2] * e[2];
-}
-
 // Static methods
-vec3 vec3::random() {
+[[nodiscard]] vec3 vec3::random() {
 	return vec3(random_double(), random_double(), random_double());
 }
 
-vec3 vec3::random(double min, double max) {
+[[nodiscard]] vec3 vec3::random(const double min, const double max) {
 	return vec3(random_double(min, max), random_double(min, max), random_double(min, max));
 }
 
@@ -93,47 +66,43 @@ vec3 vec3::random(double min, double max) {
 /////////////////////////////////////////
 
 /*-----Addition/Subtraction operators-----*/
-vec3 operator+(const vec3&u, const vec3& v) {
+[[nodiscard]] vec3 operator+(const vec3&u, const vec3& v) {
 	return vec3(u[0] + v[0], u[1] + v[1], u[2] + v[2]);
 }
 
-vec3 operator-(const vec3& u, const vec3& v) {
+[[nodiscard]] vec3 operator-(const vec3& u, const vec3& v) {
 	return vec3(u[0] - v[0], u[1] - v[1], u[2] - v[2]);
 }
 
 /*-----Multiplication operators-----*/
-vec3 operator*(double t, const vec3& v) {
+[[nodiscard]] vec3 operator*(const double t, const vec3& v) {
 	return vec3(t * v[0], t * v[1], t * v[2]);
 }
 
-vec3 operator*(const vec3& v, double t) {
+[[nodiscard]] vec3 operator*(const vec3& v, double t) {
 	return t * v;
 }
 
-vec3 operator*(const vec3& u, const vec3& v) {
+[[nodiscard]] vec3 operator*(const vec3& u, const vec3& v) {
 	return vec3(u[0] * v[0], u[1] * v[1], u[2] * v[2]);
 }
 
 /*-----Division operators-----*/
-vec3 operator/(const vec3& v1, const vec3& v2) {
+[[nodiscard]] vec3 operator/(const vec3& v1, const vec3& v2) {
 	return vec3(v1[0] / v2[0], v1[1] / v2[1], v1[2] / v2[2]);
 }
 
-vec3 operator/(vec3 v, double t) {
+[[nodiscard]] vec3 operator/(vec3 v, double t) {
 	return (1 / t) * v;
 }
 
 
 /*-----Misc. Functions-----*/
-vec3 unit_vector(vec3 v) {
+[[nodiscard]] vec3 unit_vector(vec3 v) {
 	return v / v.length();
 }
 
-double dot(const vec3& u, const vec3& v) {
-	return u[0] * v[0] + u[1] * v[1] + u[2] * v[2];
-}
-
-vec3 cross(const vec3& u, const vec3& v) {
+[[nodiscard]] vec3 cross(const vec3& u, const vec3& v) {
 	return vec3(
 		u[1] * v[2] - u[2] * v[1],
 		u[2] * v[0] - u[0] * v[2],
@@ -146,7 +115,7 @@ std::ostream& operator<<(std::ostream& output, vec3& t) {
 }
 
 /*----------Random Vector Generators----------*/
-vec3 random_in_unit_disk() {
+[[nodiscard]] vec3 random_in_unit_disk() {
 	while (true) {
 		vec3 p(random_double(-1, 1), random_double(-1, 1), 0);
 
@@ -154,7 +123,7 @@ vec3 random_in_unit_disk() {
 	}
 }
 
-vec3 random_in_unit_sphere() {
+[[nodiscard]] vec3 random_in_unit_sphere() {
 	while (true) {
 		vec3 p = vec3::random(-1, 1);
 
@@ -162,18 +131,18 @@ vec3 random_in_unit_sphere() {
 	}
 }
 
-vec3 random_unit_vector() {
+[[nodiscard]] vec3 random_unit_vector() {
 	return unit_vector(random_in_unit_sphere());
 }
 
-vec3 reflect(const vec3& v, const vec3& n) {
+[[nodiscard]] vec3 reflect(const vec3& v, const vec3& n) {
 	return v - 2 * dot(v, n) * n;
 }
 
-vec3 refract(const vec3& uv, const vec3& n, double etai_over_etat) {
-	auto cos_theta = fmin(dot(-uv, n), 1.0);
-	vec3 r_out_perp = etai_over_etat * (uv + cos_theta * n);
-	vec3 r_out_parallel = -sqrt(1.0 - r_out_perp.length_squared()) * n;
+[[nodiscard]] vec3 refract(const vec3& uv, const vec3& n, const double etai_over_etat) {
+	const auto cos_theta = fmin(dot(-uv, n), 1.0);
+	const vec3 r_out_perp = etai_over_etat * (uv + cos_theta * n);
+	const vec3 r_out_parallel = -sqrt(1.0 - r_out_perp.length_squared()) * n;
 
 	return r_out_parallel + r_out_perp;
 }
