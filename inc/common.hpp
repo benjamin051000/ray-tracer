b9382086b2a7d9dd062d5666cb506e415f4c0d3a
Benjamin Wheeler <benjaminwheeler0510@gmail.com>
2024-04-08 08:26:52 -0400

    constexpr and nodiscard qualifiers

--- inc/common.hpp
+++ inc/common.hpp
@@ -4,22 +4,29 @@
 #include <limits>
 #include <memory>
 
-using std::shared_ptr;
 using std::make_shared;
+using std::shared_ptr;
 using std::sqrt;
 
-const auto infinity = std::numeric_limits<float>::infinity();
-const auto PI = 3.1415926535897932385;
-
+constexpr auto infinity = std::numeric_limits<float>::infinity();
+constexpr auto PI = 3.1415926535897932385;
 
 // Common functions
-double deg_to_rad(double deg);
+[[nodiscard]] constexpr double deg_to_rad(const double deg) {
+	return deg * PI / 180;
+}
 
-double clamp(double x, double min, double max);
+[[nodiscard]] constexpr double clamp(const double x, const double min, const double max) {
+	if (x < min)
+		return min;
+	if (x > max)
+		return max;
+	return x;
+}
 
-double random_double();
+[[nodiscard]] double random_double();
 
-double random_double(double min, double max);
+[[nodiscard]] double random_double(const double min,
+                                             const double max);
 
-int random_int(int min, int max);
-
+[[nodiscard]] int random_int(const int min, const int max);
--- src/common.cpp
+++ src/common.cpp
@@ -1,26 +1,14 @@
 #include "common.hpp"
 
-
 // Common functions
-double deg_to_rad(double deg) {
-	return deg * PI / 180;
-}
-
-double clamp(double x, double min, double max) {
-	if (x < min) return min;
-	if (x > max) return max;
-	return x;
-}
-
-double random_double() {
+[[nodiscard]] double random_double() {
 	return rand() / (RAND_MAX + 1.0);
 }
 
-double random_double(double min, double max) {
+[[nodiscard]] double random_double(double min, double max) {
 	return min + (max - min) * random_double();
 }
 
-int random_int(int min, int max) {
+[[nodiscard]] int random_int(int min, int max) {
 	return static_cast<int>(random_double(min, max + 1));
 }
-
