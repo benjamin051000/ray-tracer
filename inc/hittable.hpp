6e4e70ab00f6f85077fd84b36f84477b9f17e838
Benjamin Wheeler <benjaminwheeler0510@gmail.com>
2024-04-26 13:18:32 -0400

    cleaned up

--- inc/hittable.hpp
+++ inc/hittable.hpp
@@ -23,7 +23,7 @@ struct hit_record {
 
 	bool front_face;
 
-	inline void set_face_normal(const ray& r, const vec3& outward_normal) {
+	constexpr void set_face_normal(const ray& r, const vec3& outward_normal) {
 		front_face = dot(r.direction, outward_normal) < 0;
 		normal = front_face ? outward_normal : -outward_normal;
 	}
@@ -32,11 +32,7 @@ struct hit_record {
 
 //ABC
 struct hittable {
-	virtual bool hit(
-		const ray& r, 
-		const double tmin, 
-		const double tmax, 
-		hit_record& rec) const = 0;
+	virtual bool hit(const ray& r, const double tmin, const double tmax, hit_record& rec) const = 0;
 
 	virtual bool bounding_box(const double t0, const double t1, aabb& output_box) const = 0;
 };
@@ -83,9 +79,14 @@ public:
 
 
 class rotate_y : public hittable {
+    shared_ptr<hittable> ptr;
+    double sin_theta, cos_theta;
+    bool hasbox = false;
+    aabb bbox;
+
 public:
     rotate_y(shared_ptr<hittable> p, double angle) : ptr(p) {
-        auto radians = deg_to_rad(angle);
+        const auto radians = deg_to_rad(angle);
         sin_theta = sin(radians);
         cos_theta = cos(radians);
         //hasbox = ptr->bounding_box(0, 1, bbox);
@@ -96,14 +97,14 @@ public:
         for (int i = 0; i < 2; i++) {
             for (int j = 0; j < 2; j++) {
                 for (int k = 0; k < 2; k++) {
-                    auto x = i * bbox.max().x() + (1 - static_cast<double>(i)) * bbox.min().x();
-                    auto y = j * bbox.max().y() + (1 - static_cast<double>(j)) * bbox.min().y();
-                    auto z = k * bbox.max().z() + (1 - static_cast<double>(k)) * bbox.min().z();
+                    const auto x = i * bbox.max().x() + (1 - static_cast<double>(i)) * bbox.min().x();
+                    const auto y = j * bbox.max().y() + (1 - static_cast<double>(j)) * bbox.min().y();
+                    const auto z = k * bbox.max().z() + (1 - static_cast<double>(k)) * bbox.min().z();
 
-                    auto newx = cos_theta * x + sin_theta * z;
-                    auto newz = -sin_theta * x + cos_theta * z;
+                    const auto newx = cos_theta * x + sin_theta * z;
+                    const auto newz = -sin_theta * x + cos_theta * z;
 
-                    vec3 tester(newx, y, newz);
+                    const vec3 tester(newx, y, newz);
 
                     for (int c = 0; c < 3; c++) {
                         min[c] = fmin(min[c], tester[c]);
@@ -118,25 +119,23 @@ public:
 
     virtual bool hit(const ray& r, double t_min, double t_max, hit_record& rec) const override {
         auto origin = r.origin;
-        auto direction = r.direction;
-
         origin[0] = cos_theta * r.origin[0] - sin_theta * r.origin[2];
         origin[2] = sin_theta * r.origin[0] + cos_theta * r.origin[2];
 
+        auto direction = r.direction;
         direction[0] = cos_theta * r.direction[0] - sin_theta * r.direction[2];
         direction[2] = sin_theta * r.direction[0] + cos_theta * r.direction[2];
 
-        ray rotated_r(origin, direction, r.time);
+        const ray rotated_r(origin, direction, r.time);
 
         if (!ptr->hit(rotated_r, t_min, t_max, rec))
             return false;
 
         auto p = rec.p;
-        auto normal = rec.normal;
-
         p[0] = cos_theta * rec.p[0] + sin_theta * rec.p[2];
         p[2] = -sin_theta * rec.p[0] + cos_theta * rec.p[2];
 
+        auto normal = rec.normal;
         normal[0] = cos_theta * rec.normal[0] + sin_theta * rec.normal[2];
         normal[2] = -sin_theta * rec.normal[0] + cos_theta * rec.normal[2];
 
@@ -151,11 +150,5 @@ public:
         return hasbox;
     }
 
-public:
-    shared_ptr<hittable> ptr;
-    double sin_theta;
-    double cos_theta;
-    bool hasbox = false;
-    aabb bbox;
 };
 
