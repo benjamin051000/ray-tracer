f487393b2462c5aa553ce2f89554cbc1e3123798
Benjamin Wheeler <benjaminwheeler0510@gmail.com>
2024-04-26 13:36:38 -0400

    refactored and `constexpr`ed aabb. It's all header-only now.

--- inc/aabb.hpp
+++ inc/aabb.hpp
@@ -1,18 +1,45 @@
 #pragma once
 #include "ray.hpp"
+#include <cmath>
 
-class aabb {
-public:
-	aabb() {}
-	aabb(const point3& a, const point3& b) : _min(a), _max(b) {}
+struct aabb {
+	point3 min, max;
 
-	point3 min() const { return _min; }
-	point3 max() const { return _max; }
+	constexpr aabb() = default;
+	constexpr aabb(const point3& a, const point3& b) : min(a), max(b) {}
 
-	bool hit(const ray& r, double tmin, double tmax) const;
+	constexpr bool hit(const ray& r, double tmin, double tmax) const { // TODO override hittable?
+		for (int a = 0; a < 3; a++) {
+			const auto invD = 1.0f / r.direction[a];
 
-	point3 _min, _max;
+			auto t0 = (min[a] - r.origin[a]) * invD;
+			auto t1 = (max[a] - r.origin[a]) * invD;
+
+			if (invD < 0.0f)
+				std::swap(t0, t1);
+
+			tmin = t0 > tmin ? t0 : tmin;
+			tmax = t1 < tmax ? t1 : tmax;
+			if (tmax <= tmin)
+				return false;
+		}
+		return true;
+	}
 };
 
 
-aabb surrounding_box(aabb box0, aabb box1);
+[[nodiscard]] constexpr aabb surrounding_box(const aabb box0, const aabb box1) {
+	const point3 small(
+		fmin(box0.min.x(), box1.min.x()),
+		fmin(box0.min.y(), box1.min.y()),
+		fmin(box0.min.z(), box1.min.z())
+	);
+
+	const point3 big(
+		fmax(box0.max.x(), box1.max.x()),
+		fmax(box0.max.y(), box1.max.y()),
+		fmax(box0.max.z(), box1.max.z())
+	);
+
+	return aabb(small, big);
+}
